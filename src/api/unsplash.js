72878f81a21ca7a85aa87d5c444136d0484d7bf8
Ben Halverson <benhalverson33@gmail.com>
2019-02-27 03:37:46 -0800

    readme update

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: `Client-ID ${process.env.REACT_APP_APIKEY}`
+    Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`
   }
 });
