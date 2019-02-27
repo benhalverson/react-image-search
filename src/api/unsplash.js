bb7000860095dfbe63f4789914959dc53fefa51c
Ben Halverson <benhalverson33@gmail.com>
2019-02-27 03:19:35 -0800

    removed api key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization:
-      'Client-ID 3b42d93b06903074b6934ad0e9e066d9c7af7c1b31b80c7f561918d1fcb5fa92'
+    Authorization: `Client-ID ${process.env.REACT_APP_APIKEY}`
   }
 });
