5e1693cb28f41446ba04526dd2c9cb7c446aa299
Ben Halverson <benhalverson33@gmail.com>
2021-08-29 16:00:58 -0700

    removed sample tests and updated search test

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -4,27 +4,12 @@ describe('Visit localhost:3000', () => {
   });
 
   it('should do a search for dogs', () => {
-    // We'll store our item text in a variable so we can reuse it
     const searchItem = 'dogs';
-
-    // Let's get the input element and use the `type` command to
-    // input our new list item. After typing the content of our item,
-    // we need to type the enter key as well in order to submit the input.
-    // This input has a data-test attribute so we'll use that to select the
-    // element in accordance with best practices:
-    // https://on.cypress.io/selecting-elements
+    
     cy.get('input').type(`${searchItem}{enter}`);
 
-    // Now that we've typed our new item, let's check that it actually was added to the list.
-    // Since it's the newest item, it should exist as the last element in the list.
-    // In addition, with the two default items, we should have a total of 3 elements in the list.
-    // Since assertions yield the element that was asserted on,
-    // we can chain both of these assertions together into a single statement.
+    cy.get('.image-list').children().should('have.length', 10);
 
-    // cy.get('.todo-list li')
-    //   .should('have.length', 3)
-    //   .last()
-    //   .should('have.text', searchItem)
   });
 });
 
